01394da1bf26b6dde64d78d69d644c09bf291183
Ethan Rice <riceethan99@gmail.com>
2026-04-08 10:49:52 +0100

    Add hover feature

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const container = document.getElementById("container")
 for(let i = 0; i < 256; i++) {
     const newDiv = document.createElement("div")
     newDiv.classList.add("grid-square")
+    newDiv.addEventListener("mouseenter", function() {
+        newDiv.style.backgroundColor = "red"
+    }) 
     container.appendChild(newDiv)
 }
 
